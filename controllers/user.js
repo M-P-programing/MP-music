e9df80961b9f9a27f79dfca190f07dfd38928c5a
Marco <marco.perrullo@gmail.com>
2020-01-23 16:58:48 +0100

    Making get Image file from user method in user controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,11 @@
 'use strict'
 
+var fs = require('fs');
+var path = require('path');
 var bcrypt = require('bcrypt-nodejs');
 var User = require('../models/user');
 var jwt = require('../services/jwt');
+
 
 function pruebas(req, res){
 	res.status(200).send({
@@ -137,10 +140,24 @@ function uploadImage(req, res){
 		res.status(200).send({message: 'No image was uploaded'});
 	}
 }
+
+function getImageFile(req,res){
+	var imageFile = req.params.imageFile;
+	var path_file = './uploads/users/' + imageFile;
+
+	fs.exists(path_file, function(exists){
+		if(exists){
+			res.sendFile(path.resolve(path_file));
+		}else{
+			res.status(200).send({message: 'Image doenst exist'});
+		}
+	});
+}
 module.exports = {
 	pruebas,
 	saveUser,
 	loginUser,
 	updateUser,
-	uploadImage
+	uploadImage,
+	getImageFile
 };
