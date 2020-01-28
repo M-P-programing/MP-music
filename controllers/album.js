2774d543dd72aee63afe5546d3db1a0cacd24da4
Marco <marco.perrullo@gmail.com>
2020-01-28 18:07:51 +0100

    Adding methods to album controller so we can save and get Albums to and from artists

---
+++ controllers/album.js
@@ -0,0 +1,80 @@
+'use strict'
+
+var path = require('path');
+var fs = require('fs');
+var mongoosePaginate = require('mongoose-pagination');
+var Artist = require('../models/artist');
+var Album = require('../models/album');
+var Song = require('../models/song');
+
+function getAlbum(req,res){
+	var albumId = req.params.id;
+
+	Album.findById(albumId).populate({path: 'artist'}).exec((err,album) =>{
+		if(err){
+			res.status(500).send({message: 'Request error'});
+		}else{
+			if(!album){
+				res.status(404).send({message: 'No album found'});
+			}else{
+				res.status(200).send({album});
+			}
+		}
+	});
+}
+
+function saveAlbum(req,res){
+	var album = new Album();
+
+	var params = req.body;
+	album.title = params.title;
+	album.description = params.description;
+	album.year = params.year;
+	album.image = 'null';
+	album.artist = params.artist;
+
+	album.save((err,albumStored) =>{
+		if(err){
+			res.status(500).send({message: 'Error connecting to server'});
+		}else{
+			if(!albumStored){
+				res.status(404).send({message: 'Album wasnt saved in the database'});
+			}else{
+				res.status(200).send({album:albumStored});
+			}
+		}
+	});
+
+}
+
+function getAlbums(req,res){
+	var artistId = req.params.artist;
+
+	if(!artistId){
+		// Get all albums from db
+
+		var find = Album.find({}).sort('title');
+	}else{
+		// Get albums just from artist id
+		
+		var find = Album.find({artist: artistId}).sort('year');
+	}
+
+	find.populate({path: 'artist'}).exec((err,albums)=>{
+		if(err){
+			res.status(500).send({message: 'Error connecting to server'});
+		}else{
+			if(!albums){
+				res.status(404).send({message: 'No albums found'});
+			}else{
+				res.status(200).send({albums});
+			}
+		}
+	});
+}
+
+module.exports = {
+	getAlbum,
+	saveAlbum,
+	getAlbums
+};
