8dbe41a4b968ef415cfc3268e6435cd7bec54eec
Marco <marco.perrullo@gmail.com>
2020-01-30 17:36:39 +0100

    Finishing methods for Song controller and adding headers to app

--- controllers/song.js
+++ controllers/song.js
@@ -8,7 +8,18 @@ var Album = require('../models/album');
 var Song = require('../models/song');
 
 function getSong(req,res){
-	res.status(200).send({message: 'Test'});
+	var songId = req.params.id;
+	Song.findById(songId).populate({path: 'album'}).exec((err, song) => {
+		if(err){
+			res.status(500).send({message: 'Error getting data from database'});
+		}else{
+			if(!song){
+				res.status(404).send({message: 'Song doens\'t exist'});
+			}else{
+				res.status(200).send({song});
+			}
+		}
+	});
 }
 
 function saveSong(req,res){
@@ -34,7 +45,112 @@ function saveSong(req,res){
 	});
 }
 
+function getSongs(req,res){
+	var albumId = req.params.album;
+
+	if(!albumId){
+		var find = Song.find({}).sort('number');
+	}else{
+		var find = Song.find({album: albumId}).sort('number');
+	}
+
+	find.populate({
+		path: 'album',
+		populate: {
+			path: 'artist',
+			model: 'Artist'
+		}
+	}).exec(function(err,songs){
+		if(err){
+			res.status(500).send({message: 'Server error'});
+		}else{
+			if(!songs){
+				res.status(404).send({message: 'No songs found'});
+			}else{
+				res.status(200).send({songs});
+			}
+		}
+	});
+}
+
+function updateSong(req,res){
+	var songId = req.params.id;
+	var update = req.body;
+
+	Song.findByIdAndUpdate(songId, update, (err,songUpdated) =>{
+		if(err){
+			res.status(500).send({message: 'Server error'});
+		}else{
+			if(!songUpdated){
+				res.status(404).send({message: 'The song wasn\'t updated'});
+			}else{
+				res.status(200).send({songUpdated});
+			}
+		}
+	});
+}
+
+function deleteSong(req,res){
+	var songId = req.params.id;
+	Song.findByIdAndDelete(songId,(err, songRemoved) =>{
+		if(err){
+			res.status(500).send({message: 'Server error'});
+		}else{
+			if(!songRemoved){
+				res.status(404).send({message: 'The song wasn\'t deleted'});
+			}else{
+				res.status(200).send({songRemoved});
+			}
+		}
+	});
+}
+
+function uploadFile(req, res){
+	var songId = req.params.id;
+	var file_name = 'File not uploaded';
+
+	if(req.files){
+		var file_path = req.files.file.path;
+		var file_split = file_path.split('\/');
+		var file_name = file_split[2];
+		var ext_split = file_name.split('\.');
+		var file_ext = ext_split[1];
+
+		if(file_ext == 'mp3' || file_ext == 'ogg' ){
+			
+			Song.findByIdAndUpdate(songId, {file: file_name}, (err,songUpdated) => {
+				if(!songUpdated){
+					res.status(404).send({message: "Something went wrong. Song wasn't updated."});
+				}else{
+					res.status(200).send({song: songUpdated});
+				}
+			});
+		}else{
+			res.status(200).send({message: 'File extension is not correct'});
+		}
+	}else{
+		res.status(200).send({message: 'No song was uploaded'});
+	}
+}
+
+function getSongFile(req,res){
+	var songFile = req.params.songFile;
+	var path_file = './uploads/songs/' + songFile;
+
+	fs.exists(path_file, function(exists){
+		if(exists){
+			res.sendFile(path.resolve(path_file));
+		}else{
+			res.status(200).send({message: 'Song doesn\'t exist'});
+		}
+	});
+}
 module.exports = {
 	getSong,
-	saveSong
+	saveSong,
+	getSongs,
+	updateSong,
+	deleteSong,
+	uploadFile,
+	getSongFile
 }
