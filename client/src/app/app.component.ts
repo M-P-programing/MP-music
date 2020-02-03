8f4afcf37b86b6239a14c21b23b43e5c85f051c8
Marco <marco.perrullo@gmail.com>
2020-02-03 14:42:58 +0100

    Login and register forms

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [
+  './app.component.scss',
+  ]
 })
 export class AppComponent {
-  title = 'client';
+  public title = 'MP-Music';
+  public user: User;
+  public identity;
+  public token;
+
+  constructor(){
+  	this.user = new User('','','','','','ROLE_USER','');
+  }
 }
