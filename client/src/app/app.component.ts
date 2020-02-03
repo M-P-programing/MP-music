4990a42daaf82464914abf20f9d4ca62e6254c02
Marco <marco.perrullo@gmail.com>
2020-02-03 15:39:18 +0100

    Adding method signup to user service

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,20 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { User } from './models/user';
+import { UserService } from './services/user.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [
-  './app.component.scss',
-  ]
+  styleUrls: ['./app.component.scss'],
+  providers: [UserService]
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
   public title = 'MP-Music';
   public user: User;
   public identity;
   public token;
 
-  constructor(){
+  constructor(private _userService:UserService){
   	this.user = new User('','','','','','ROLE_USER','');
+  }
+
+ngOnInit(){
+ 
+}
+  public onSubmit(){
+    console.log(this.user);
   }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +15,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    HttpModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent],
